591c785ee23c231b9a05343b9f3095df249692a9
Max Eliseev <mxseev@gmail.com>
2022-07-07 21:40:29 +0500

    fix: корректный импорт Sentry

--- packages/monitoring/src/next.ts
+++ packages/monitoring/src/next.ts
@@ -1,10 +1,12 @@
-import Sentry, {withSentry} from "@sentry/nextjs"
+import * as Sentry from "@sentry/nextjs"
 
-type NextApiHandler = Parameters<typeof withSentry>[0]
+type NextApiHandler = Parameters<typeof Sentry.withSentry>[0]
 
 const withSentryFlush = (handler: NextApiHandler): NextApiHandler => {
+  const wrappedHandler = Sentry.withSentry(handler)
+
   return async (request, response) => {
-    const returns = await withSentry(handler)(request, response)
+    const returns = await wrappedHandler(request, response)
 
     // we need do it manually in Vercel
     // https://github.com/vercel/next.js/blob/canary/examples/with-sentry/pages/api/test4.js#L10-L12
@@ -15,4 +17,4 @@ const withSentryFlush = (handler: NextApiHandler): NextApiHandler => {
 }
 
 export {default, withSentryConfig} from "@sentry/nextjs"
-export {withSentryFlush as withSentry}
+export {withSentryFlush as withSentry, Sentry}
