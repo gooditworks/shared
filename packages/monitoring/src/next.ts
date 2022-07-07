aa2af48bb1e87a5d6630726bc6059a88c5a27731
Max Eliseev <mxseev@gmail.com>
2022-07-07 21:15:30 +0500

    fix: вызов Sentry.flush в withSentry

--- packages/monitoring/src/next.ts
+++ packages/monitoring/src/next.ts
@@ -1,1 +1,16 @@
-export {withSentry, withSentryConfig} from "@sentry/nextjs"
+import Sentry, {withSentry} from "@sentry/nextjs"
+
+type NextApiHandler = Parameters<typeof withSentry>[0]
+
+const withSentryFlush = async (handler: NextApiHandler) => {
+  const response = await withSentry(handler)
+
+  // we need do it manually in Vercel
+  // https://github.com/vercel/next.js/blob/canary/examples/with-sentry/pages/api/test4.js#L10-L12
+  await Sentry.flush(2500)
+
+  return response
+}
+
+export {default, withSentryConfig} from "@sentry/nextjs"
+export {withSentryFlush as withSentry}
