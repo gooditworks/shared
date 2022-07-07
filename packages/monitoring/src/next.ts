3cbb3c2ece48f3606189b022db56c86c718df0cb
Max Eliseev <mxseev@gmail.com>
2022-07-07 21:31:44 +0500

    fix: корректный withSentry

--- packages/monitoring/src/next.ts
+++ packages/monitoring/src/next.ts
@@ -2,14 +2,16 @@ import Sentry, {withSentry} from "@sentry/nextjs"
 
 type NextApiHandler = Parameters<typeof withSentry>[0]
 
-const withSentryFlush = async (handler: NextApiHandler) => {
-  const response = await withSentry(handler)
+const withSentryFlush = (handler: NextApiHandler): NextApiHandler => {
+  return async (request, response) => {
+    const returns = await withSentry(handler)(request, response)
 
-  // we need do it manually in Vercel
-  // https://github.com/vercel/next.js/blob/canary/examples/with-sentry/pages/api/test4.js#L10-L12
-  await Sentry.flush(2500)
+    // we need do it manually in Vercel
+    // https://github.com/vercel/next.js/blob/canary/examples/with-sentry/pages/api/test4.js#L10-L12
+    await Sentry.flush(2500)
 
-  return response
+    return returns
+  }
 }
 
 export {default, withSentryConfig} from "@sentry/nextjs"
