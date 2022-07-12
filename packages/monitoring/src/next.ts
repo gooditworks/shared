af53628c0786a4f6579d4bb39525ca113a8069a2
Max Eliseev <mxseev@gmail.com>
2022-07-13 03:39:50 +0500

    refactor: удалена ненужная обёртка withSentry

--- packages/monitoring/src/next.ts
+++ packages/monitoring/src/next.ts
@@ -1,20 +1,6 @@
 import * as Sentry from "@sentry/nextjs"
 
-type NextApiHandler = Parameters<typeof Sentry.withSentry>[0]
+const {withSentry, withSentryConfig} = Sentry
 
-const withSentryFlush = (handler: NextApiHandler): NextApiHandler => {
-  const wrappedHandler = Sentry.withSentry(handler)
-
-  return async (request, response) => {
-    const returns = await wrappedHandler(request, response)
-
-    // we need do it manually in Vercel
-    // https://github.com/vercel/next.js/blob/canary/examples/with-sentry/pages/api/test4.js#L10-L12
-    await Sentry.flush(2500)
-
-    return returns
-  }
-}
-
-export {default, withSentryConfig} from "@sentry/nextjs"
-export {withSentryFlush as withSentry, Sentry}
+export default Sentry
+export {withSentry, withSentryConfig}
